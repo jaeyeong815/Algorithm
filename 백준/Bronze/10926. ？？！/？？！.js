5e714a427d67f7619bf6c045e323b2fa4c632ebb
Jaeyeong Ji <jae0ee@naver.com>
2023-01-28 13:44:10 +0900

    [Bronze V] Title: ??!, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+input = input[0];
+solution(input);
+
+function solution(id) {
+  console.log(`${id}??!`);
+}
