821f17775ba3e9416a15a0e7f57daa2a9832894c
Jaeyeong Ji <jae0ee@naver.com>
2023-02-09 14:00:21 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,13 +1,7 @@
 function solution(chicken) {
     let service = 0;
-    let rest = 0;
-    
-    while (chicken > 1) {
-      service += Math.floor(chicken / 10);
-      rest += Math.floor(chicken % 10);
-      chicken /= 10;
-    }
-    
+    let rest = chicken;
+
     while (rest >= 10) {
       service += Math.floor(rest / 10);
       rest = Math.floor(rest % 10) + Math.floor(rest / 10);
