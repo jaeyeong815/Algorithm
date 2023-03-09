e3168608df9e02b58ba46862b8ad52bd53c4d52b
Jaeyeong Ji <jae0ee@naver.com>
2023-03-09 10:59:00 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -0,0 +1,8 @@
+function solution(array, commands) {
+    const answer = [];
+    commands.forEach(command => {
+        const [i,j,k] = command;
+        answer.push(array.slice(i-1, j).sort((a,b)=>a-b)[k-1]);
+    })
+    return answer;
+}
