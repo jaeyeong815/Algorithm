9e5d1b1ee2ba223edbf316712b67f014883b1a8a
Jaeyeong Ji <jae0ee@naver.com>
2023-03-14 21:42:07 +0900

    [level 1] Title: 예산, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,17 +1,8 @@
 function solution(d, budget) {
-    // 계산하기 편하게 오름차순 정렬
     d = d.sort((a,b)=>a-b);
-    
-    // 예산의 총 합을 구한다
     let sum = d.reduce((cal, cur) => cal += cur,0);
-    
-    // 총 합과 예산을 비교한다
-    // 같거나 예산이 더 크면 배열의 길이를 반환
     if (sum <= budget) return d.length;
-    
-    // 예산이 더 작다면 값을 하나씩 빼면서 비교한다
     let min;
-       
     while(sum > budget) {
         let subArr = d.map((el) => sum - el);
         min = Math.min(...subArr);
@@ -23,6 +14,5 @@ function solution(d, budget) {
         }
         sum = min
     }
-    
     return d.length;
 }
