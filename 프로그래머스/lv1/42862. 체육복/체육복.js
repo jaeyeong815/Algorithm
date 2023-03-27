9c85c5068eda1b757a703f82abbe2b34525c6b36
Jaeyeong Ji <jae0ee@naver.com>
2023-03-27 10:40:12 +0900

    [level 1] Title: 체육복, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -0,0 +1,21 @@
+function solution(n, lost, reserve) {
+    let o = n - lost.length;
+    reserve.sort().forEach((el, idx) => {
+        if (lost.includes(el)) {
+            lost = lost.filter(n => n !== el);
+            reserve = reserve.filter(n => n !== el);
+            o += 1;
+        }
+    });
+    lost.sort().forEach(el => {
+        if (reserve.includes(el-1)) {
+            o += 1;
+            reserve = reserve.filter(n => n !== el-1);
+        }
+        else if (reserve.includes(el+1)) {
+            o += 1;
+            reserve = reserve.filter(n => n !== el+1);
+        }
+    })
+    return o;
+}
