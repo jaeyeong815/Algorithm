f67a3e2d772eaaae21dba0ac2b7f1f2bcd4abe97
Jaeyeong Ji <jae0ee@naver.com>
2023-03-29 09:34:01 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,7 +1,8 @@
 function solution(n, arr1, arr2) {
     const a = [];
-
+    // 정수를 이진수로, n자릿수 수가 아니면 앞자리를 0으로 채우기
     arr1 = arr1.map(el => el.toString(2).padStart(n, 0)).map(el => {
+        // 0이면 #, 1이면 공백을 치환하여 반환하기
         let string = '';
         for (const str of el) {
             str === '1' ? string += '#' : string += ' '
@@ -15,7 +16,8 @@ function solution(n, arr1, arr2) {
         }
         return string
     });
-
+    
+    // 반복문 돌면서 arr1, arr2 둘 중 하나라도 #이면 #, 둘 다 공백이면 공백을 반환하여 답안 제출하기
     for (let i = 0; i<n; i++) {
         let p = '';
         for (let j = 0; j<n; j++) {
