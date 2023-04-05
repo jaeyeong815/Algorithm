aee35b2619fc8cb89f5ad2aba0fc60d21957f691
Jaeyeong Ji <jae0ee@naver.com>
2023-04-05 13:39:17 +0900

    [level 2] Title: 위장, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.js
+++ 프로그래머스/lv2/42578. 위장/위장.js
@@ -1,18 +1,10 @@
 function solution(clothes) {
     const obj = {};
-    const length = [];
-    let answer = 0;
+    
     for(let el of clothes) {
         const [name, kind] = el;
-        obj[kind] ? obj[kind].push(name) : obj[kind] = [name];
+        obj[kind] ? obj[kind] += 1 : obj[kind] = 1;
     }
     
-    if (Object.values(obj).length === 1) return Object.values(obj)[0].length;
-
-    for (let el of Object.values(obj)) {
-        length.push(el.length)
-    }
-    
-    const t = length.reduce((cal, cur) => cal *= (cur+1),1);
-    return t-1
+    return Object.values(obj).reduce((cal, cur) => cal *= (cur+1),1) - 1
 }
